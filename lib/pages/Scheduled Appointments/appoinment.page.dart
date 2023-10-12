7764bdc3ec88d7c6cf7090407c830cb491ba5b6c
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2023-10-12 22:24:06 +0700

    Menambahkan tab scheduled appointment
    3 status appointment, upcoming, completed, cancelled.
    template card scheduled appointment (Uncontrolled).

--- lib/pages/Scheduled Appointments/appoinment.page.dart
+++ lib/pages/Scheduled Appointments/appoinment.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:informaticconnect_app/config/app.color.dart';
 
 class Appointment extends StatefulWidget {
   const Appointment({super.key});
@@ -7,27 +9,27 @@ class Appointment extends StatefulWidget {
   State<Appointment> createState() => _AppointmentState();
 }
 
-enum FilterStatus {upcoming, completed, cancelled}
+enum FilterStatus {Upcoming, Completed, Cancelled}
 
 class _AppointmentState extends State<Appointment> {
-  FilterStatus status = FilterStatus.upcoming;
+  FilterStatus status = FilterStatus.Upcoming;
   Alignment _alignment = Alignment.centerLeft;
 
   List<dynamic> schedules = [
     {
       "mentor_name":"Nizam Setiawan",
       "mentor_profile":"asset_image",
-      "status":FilterStatus.upcoming,
+      "status":FilterStatus.Upcoming,
     },
     {
       "mentor_name":"Nizam Setiawan",
       "mentor_profile":"asset_image",
-      "status":FilterStatus.completed,
+      "status":FilterStatus.Completed,
     },
     {
       "mentor_name":"Nizam Setiawan",
       "mentor_profile":"asset_image",
-      "status":FilterStatus.cancelled,
+      "status":FilterStatus.Cancelled,
     },
   ];
   
@@ -40,16 +42,199 @@ class _AppointmentState extends State<Appointment> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(24),
-        child: ListView(
-          children: [
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
             Container(
-              padding: EdgeInsets.symmetric(vertical: 12),
+              padding: EdgeInsets.only(top: 36, bottom: 12),
               child: Text('My Appointment', style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.w700,
                 fontSize: 24,
               ),),
             ),
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for (FilterStatus filterStatus in FilterStatus.values)
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (filterStatus == FilterStatus.Upcoming) {
+                                  status = FilterStatus.Upcoming;
+                                  _alignment = Alignment.centerLeft;
+                                } else if (filterStatus == FilterStatus.Completed) {
+                                  status = FilterStatus.Completed;
+                                  _alignment = Alignment.center;
+                                } else if (filterStatus == FilterStatus.Cancelled) {
+                                  status = FilterStatus.Cancelled;
+                                  _alignment = Alignment.centerRight;
+                                }
+                              });
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(vertical: 12),
+                              child: Center(
+                                child: Text(
+                                  filterStatus.name,
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              )
+                            ),
+                          )
+                        )
+                      ],
+                    ),
+                    Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 12, bottom: 12),
+                  child: AnimatedAlign(
+                    alignment: _alignment, 
+                    duration: Duration(milliseconds: 00),
+                    child: Container(
+                      // padding: EdgeInsets.symmetric(horizontal: 8),
+                      decoration: BoxDecoration(
+                        color: AppColor.bgscaffolod
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            status.name,
+                            style: TextStyle(
+                              color: AppColor.buttoncolor,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 12),
+                            width: 115,
+                            height: 2,
+                            color: AppColor.buttoncolor,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredSchedules.length,
+                itemBuilder: ((context, index) {
+                  var _scheduled = filteredSchedules[index];
+                  bool isLastElement = filteredSchedules.length + 1 == index;
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    margin: !isLastElement 
+                    ? EdgeInsets.only(bottom: 24)
+                    : EdgeInsets.zero,
+                    color: Color.fromARGB(255, 40, 44, 57),
+                    child: Container(
+                      padding: EdgeInsets.all(12),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 12),
+                                width: 80,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.blueGrey
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    _scheduled['mentor_name'],
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.white,
+                                      fontSize: 18
+                                    ),
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Messaging - ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Container(
+                                        child: Text(
+                                          status.name,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Tanggal | ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Jam',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Spacer(),
+                              IconButton(
+                                onPressed: () {}, 
+                                icon: Icon(
+                                  Icons.message_rounded, 
+                                  color: Colors.blue
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                })
+              ),
+            )
           ],
         ),
       ),
