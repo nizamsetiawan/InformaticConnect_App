0b53362339fdbb121465db3030b3d75183a3a6e2
aliyalfita <aliyahalfita.21013@mhs.unesa.ac.id>
2023-10-13 12:25:51 +0700

    artikel-trending slider
    membuat trending slider halaman artikel

--- lib/config/app.color.dart
+++ lib/config/app.color.dart
@@ -4,4 +4,6 @@ class AppColor {
   static const bgscaffolod = Color(0xff181A20);
   static const buttoncolor = Color(0xffFF731D);
   static const white = Colors.white;
+
+  static var bgwidget;
 }
---
+++ lib/pages/artikel/artikel.slider.dart
@@ -0,0 +1,15 @@
+//import 'package:flutter/material.dart';
+//import 'package:informaticconnect_app/pages/artikel/artikel.page.dart';
+
+//class ArtikelSlider extends StatelessWidget {
+//const ArtikelSlider({super.key});
+
+//@override
+//State<ArtikelPage> createState() => _ArtikelSliderState();
+//}
+//class _ArtikelSliderState extends State<ArtikelSlider> {
+//@override
+//Widget build(BuildContext context) {
+///return Container();
+//}
+//}
